90633a4d7936ac14fa56eab8d7de8257c90c2c29
Herindrainyall2003 <herindrainyall2003@gmail.com>
2024-12-07 12:31:26 +0100

    initialization of the project

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { dbConfig } from 'dbConfig';
 
 
 @Module({
-  imports: [TypeOrmModule.forRoot(dbConfig)],
   controllers: [AppController],
   providers: [AppService],
 })
